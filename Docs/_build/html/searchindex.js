2a09916b40b47be7a034d26046baab8a70c63d0c
Joanne Budzien <jbudzien@lanl.gov>
2017-08-10 10:57:45 -0600

    Add the Sphinx documentation.
    At this point, all that happens is a template is generated.  However, the
    template works just fine.

---
+++ Docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Finite Difference Method&#8217;s documentation!"],titleterms:{differ:0,document:0,finit:0,indice:0,method:0,tabl:0,welcom:0}})
